c1d658b0598e81ad9389940e412de177bac05bae
paulagimeno <paula-gl-98@hotmail.com>
2023-11-14 16:16:12 +0100

    slight change

--- src/api/controllers/pets.controllers.js
+++ src/api/controllers/pets.controllers.js
@@ -1,18 +1,28 @@
 const Pet = require("../models/pet.model");
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET,
+});
 
 const registerPet = async (req, res)=> {
     try {
         const body = req.body
         const pet = new Pet(body)
         if(req.file && req.file.path){
-            pet.image = req.file.path;
+
+            const cloudinaryResponse = await cloudinary.uploader.upload(req.file.path);
+            pet.image = cloudinaryResponse.secure_url;
         }
 
         const createdpet = await pet.save();
         return res.status(201).json(createdpet)
     } catch (error) {
         console.error(error);
-        return res.status(500).json('there is big mistake');
+
+        return res.status(500).json({ error: 'Internal server error' });
     }
 };
 
